a792267f920a592fc804ff8ef2da050772989d08
AxelLedung <axel.ledung@gmail.com>
2024-04-13 17:18:08 +0200

    LibraryWebsite Patch 0.8

--- LibraryWebsite/scripts/library.js
+++ LibraryWebsite/scripts/library.js
@@ -1,6 +1,5 @@
 const bookShowcase = document.getElementById("books-showcase-container");
 
-
 bookShowcase.addEventListener("click", function(event) {
     let target = event.target;
     let bookTarget;
@@ -32,6 +31,7 @@ bookShowcase.addEventListener("click", function(event) {
         window.location.reload();
     }
 });
+
 function createBooksFromJsonObject(jsonObjects) {
     bookList = [];
     for (let i in jsonObjects) {
@@ -45,58 +45,61 @@ function createBooksFromJsonObject(jsonObjects) {
         ))
     }
 }
+
 function showBooks() {
-    for (let i in bookList)
-    {
-        console.log(i);
-        let bookContainer = document.createElement("div");
-        bookContainer.classList.add("book-container");
+    if (bookList.length > 0) {
+        for (let i in bookList)
+        {
+            console.log(i);
+            let bookContainer = document.createElement("div");
+            bookContainer.classList.add("book-container");
         
-        let bookImageContainer = document.createElement("div");
-        bookImageContainer.classList.add("book-image-container");
-        bookContainer.appendChild(bookImageContainer)
+            let bookImageContainer = document.createElement("div");
+            bookImageContainer.classList.add("book-image-container");
+            bookContainer.appendChild(bookImageContainer)
 
-        let bookTitle = document.createElement("div")
-        bookTitle.classList.add("book-title");
-        bookTitle.innerHTML = bookList[i].title;
-        bookContainer.appendChild(bookTitle);
+            let bookTitle = document.createElement("div")
+            bookTitle.classList.add("book-title");
+            bookTitle.innerHTML = bookList[i].title;
+            bookContainer.appendChild(bookTitle);
 
-        let bookAuthor = document.createElement("div");
-        bookAuthor.classList.add("book-author");
-        bookAuthor.innerHTML = bookList[i].author;
-        bookContainer.appendChild(bookAuthor);
-        bookShowcase.appendChild(bookContainer);
+            let bookAuthor = document.createElement("div");
+            bookAuthor.classList.add("book-author");
+            bookAuthor.innerHTML = bookList[i].author;
+            bookContainer.appendChild(bookAuthor);
+            bookShowcase.appendChild(bookContainer);
 
         
-        if (!bookList[i].loaned) {
-            let loanBookButton = document.createElement("button");
-            loanBookButton.innerHTML = "Loan book!";
-            loanBookButton.classList.add("loan-book-button");
-            bookContainer.appendChild(loanBookButton);
+            if (!bookList[i].loaned) {
+                let loanBookButton = document.createElement("button");
+                loanBookButton.innerHTML = "Loan book!";
+                loanBookButton.classList.add("loan-book-button");
+                bookContainer.appendChild(loanBookButton);
+            }
+            else {
+                let loanBookButton = document.createElement("button");
+                loanBookButton.innerHTML = "Loaned!";
+                bookContainer.appendChild(loanBookButton);
+                loanBookButton.setAttribute("disabled", "")
+            }
+
+            let bookId = document.createElement("input");
+            bookId.classList.add("book-id");
+            bookId.setAttribute("type", "hidden");
+            bookId.setAttribute("value", bookList[i].id);
+            bookContainer.appendChild(bookId);
+
+            let bookPageCount = document.createElement("input");
+            bookPageCount.classList.add("book-pagecount");
+            bookPageCount.setAttribute("type", "hidden");
+            bookPageCount.setAttribute("value", bookList[i].pageCount);
+            bookContainer.appendChild(bookPageCount);
         }
-        else {
-            let loanBookButton = document.createElement("button");
-            loanBookButton.innerHTML = "Loaned!";
-            bookContainer.appendChild(loanBookButton);
-            loanBookButton.setAttribute("disabled", "")
-        }
-
-        /*let returnBookButton = document.createElement("button");
-        returnBookButton.innerHTML = "Return book!";
-        returnBookButton.classList.add("return-book-button");
-        bookContainer.appendChild(returnBookButton);*/
-
-        let bookId = document.createElement("input");
-        bookId.classList.add("book-id");
-        bookId.setAttribute("type", "hidden");
-        bookId.setAttribute("value", bookList[i].id);
-        bookContainer.appendChild(bookId);
-
-        let bookPageCount = document.createElement("input");
-        bookPageCount.classList.add("book-pagecount");
-        bookPageCount.setAttribute("type", "hidden");
-        bookPageCount.setAttribute("value", bookList[i].pageCount);
-        bookContainer.appendChild(bookPageCount);
+    }
+    else {
+        let noBooksText = document.createElement("h2");
+        noBooksText.innerHTML = "Oh no, someone have stolen all the books in the library.";
+        bookShowcase.appendChild(noBooksText);
     }
 }
 function onLoad() {
@@ -110,9 +113,6 @@ function onLoad() {
         console.log(request.response);
         createBooksFromJsonObject(request.response);
         showBooks();
-
-        console.log("username is: " + localStorage.getItem("username"));
-
     }
 } 
 
--- LibraryWebsite/scripts/yourbooks.js
+++ LibraryWebsite/scripts/yourbooks.js
@@ -1,5 +1,33 @@
 const bookShowcase = document.getElementById("books-showcase-container");
 
+bookShowcase.addEventListener("click", function(event) {
+    let target = event.target;
+    let bookTarget;
+    if (target.classList.contains("return-book-button")) {
+        bookTarget = target.parentElement;
+    }
+
+    let bookId = bookTarget.getElementsByClassName("book-id")[0].value;
+
+    //let bookTitle = bookTarget.getElementsByClassName("book-title")[0].innerHTML;
+    //console.log(bookTitle);
+    //let bookAuthor = bookTarget.getElementsByClassName("book-author")[0].innerHTML;
+    //console.log(bookAuthor);
+    //let bookPageCount = bookTarget.getElementsByClassName("book-pagecount")[0].value;
+    //console.log(bookPageCount);
+    
+    let request = new XMLHttpRequest();
+    request.open("PUT", "http://localhost:8080/book/returnBook?id=" + bookId + "&id=" + bookId);
+    request.responseType = "json";
+    request.send(); 
+    request.onload = function() {
+        console.log("We have got our response!");
+        console.log("Status code: " + request.status);
+        console.log(request.response);
+        window.location.reload();
+    }
+});
+
 function createBooksFromJsonObject(jsonObjects) {
     bookList = [];
     for (let i in jsonObjects) {
@@ -7,35 +35,60 @@ function createBooksFromJsonObject(jsonObjects) {
             jsonObjects[i].id,
             jsonObjects[i].title,
             jsonObjects[i].author,
-            jsonObjects[i].pageCount
+            jsonObjects[i].pageCount,
+            jsonObjects[i].loaned,
+            jsonObjects[i].loanee
         ))
     }
 }
 function showBooks() {
-    for (let i in bookList)
-    {
-        console.log(i);
-        let bookContainer = document.createElement("div");
-        bookContainer.classList.add("book-container");
+    if (bookList.length > 0) {
+        for (let i in bookList) {
+            let bookContainer = document.createElement("div");
+            bookContainer.classList.add("book-container");
         
-        let bookImageContainer = document.createElement("div");
-        bookImageContainer.classList.add("book-image-container");
-        bookContainer.appendChild(bookImageContainer);
+            let bookImageContainer = document.createElement("div");
+            bookImageContainer.classList.add("book-image-container");
+            bookContainer.appendChild(bookImageContainer);
 
-        let bookTitle = document.createElement("div")
-        bookTitle.classList.add("book-title");
-        bookTitle.innerHTML = bookList[i].title;
-        bookContainer.appendChild(bookTitle);
+            let bookTitle = document.createElement("div")
+            bookTitle.classList.add("book-title");
+            bookTitle.innerHTML = bookList[i].title;
+            bookContainer.appendChild(bookTitle);
 
-        let bookAuthor = document.createElement("div");
-        bookAuthor.classList.add("book-author");
-        bookAuthor.innerHTML = bookList[i].author;
-        bookContainer.appendChild(bookAuthor);
-        bookShowcase.appendChild(bookContainer);
+            let bookAuthor = document.createElement("div");
+            bookAuthor.classList.add("book-author");
+            bookAuthor.innerHTML = bookList[i].author;
+            bookContainer.appendChild(bookAuthor);
+            bookShowcase.appendChild(bookContainer);
+
+            let returnBookButton = document.createElement("button");
+            returnBookButton.innerHTML = "Return book!";
+            returnBookButton.classList.add("return-book-button");
+            bookContainer.appendChild(returnBookButton);
+
+            let bookId = document.createElement("input");
+            bookId.classList.add("book-id");
+            bookId.setAttribute("type", "hidden");
+            bookId.setAttribute("value", bookList[i].id);
+            bookContainer.appendChild(bookId);
+
+            let bookPageCount = document.createElement("input");
+            bookPageCount.classList.add("book-pagecount");
+            bookPageCount.setAttribute("type", "hidden");
+            bookPageCount.setAttribute("value", bookList[i].pageCount);
+            bookContainer.appendChild(bookPageCount);
+        }
+    }
+    else {
+        let noBooksText = document.createElement("h2");
+        noBooksText.innerHTML = "You have not loaned any books!";
+        bookShowcase.appendChild(noBooksText);
     }
 }
-let user = encodeURIComponent(localStorage.getItem("username"));
-let request = new XMLHttpRequest();
+function onLoad() {
+    let user = encodeURIComponent(localStorage.getItem("username"));
+    let request = new XMLHttpRequest();
     request.open("GET", "http://localhost:8080/book/bookByLoaner?loanee=" + user);
     request.responseType = "json";
     request.send(); 
@@ -45,4 +98,7 @@ let request = new XMLHttpRequest();
         console.log(request.response);
         createBooksFromJsonObject(request.response);
         showBooks();
+    }   
 }
+
+onLoad();
